0393f8c80b95a37149717c2404ef5dced5357cd4
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-04-24 15:27:18 +0300

    added User and Vehicle entities with migrations

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -10,5 +10,7 @@ namespace TanksAPI.Data
         }
 
         public DbSet<User> Users { get; set; } 
+        public DbSet<Vehicle> Vehicles { get; set; }
+      
     }
 }
---
+++ Vehicle.cs
@@ -0,0 +1,11 @@
+﻿namespace TanksAPI
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string VehicleType { get; set; } = "SPG";
+        public User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
